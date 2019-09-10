3a0e7032c2d38716cea5b692e263e0f490133697
kyle <kyle.mcinness@my.jcu.edu.au>
2019-09-10 17:10:26 +1000

    Declare variables and create Setters/Getters

--- src/Road.java
+++ src/Road.java
@@ -1,4 +1,21 @@
+import java.util.Random;
 
 public class Road {
+    private int length;
 
+    public void setLength(int car_length) //  sets the road to be a random length between 6 and 15
+    {
+        int bus_length = car_length*3; // Sets bus_length to 3 times car_length
+        Random random = new Random();
+        int random_int = random.nextInt(bus_length*5); // Gets a random int between 0 and 15
+        while (random_int < bus_length*2) // While the int is below the minimum road_length
+            random_int = random.nextInt(bus_length*5);
+        this.length = ++random_int; // Assigns the int to the length of the road
+
+    }
+
+    public int getLength()
+    {
+        return this.length;
+    }
 }
--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,5 +1,41 @@
-
+import java.util.Random;
 
 public class TrafficLight {
+    private int position;
+    private boolean status;
+    private double change_rate;
+
+    public TrafficLight() {
+        this.position = 7;
+        this.status = false;
+        this.change_rate = 0.3;
+    }
+
+    // Setters and Getters
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setStatus()
+    {
+        Random random = new Random();
+        double random_int = random.nextDouble(); // Generates a random double
+
+        while (random_int >= this.change_rate && !this.status) { // While the random double is greater than the change rate and the status is false
+            random_int = random.nextDouble(); // Generate a new random double
+        }
+        this.status = true;
+    }
+
+    public int getPosition()
+    {
+        return this.position;
+    }
+
+    public boolean isStatus()
+    {
+        return this.status;
+    }
 
 }
