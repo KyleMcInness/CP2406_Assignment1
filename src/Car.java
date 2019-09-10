17644a93e94af497cba0f54c267ede626b271a07
kyle <kyle.mcinness@my.jcu.edu.au>
2019-09-10 17:08:18 +1000

    Declare variables and create Setters/Getters

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,33 @@
 class Car {
+    // Declare variables
+    private int position;
+    private int length;
 
+    public Car()
+    {
+        this.position = 1;
+        this.length = 1;
+    }
+
+    public void move() // Moves the car 1 position forward
+    {
+        this.position++;
+    }
+
+    // Setters and Getters
+
+    public void setPosition(int length)
+    {
+        this.position = length;
+    }
+
+    public int getLength()
+    {
+        return this.length;
+    }
+
+    public int getPosition()
+    {
+        return this.position;
+    }
 }
