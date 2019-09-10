b8900631fd29561669d7862eae0512c4b5934b0c
kyle <kyle.mcinness@my.jcu.edu.au>
2019-09-10 18:06:12 +1000

    Add simulation code

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,18 @@ public class Main
         System.out.printf("1st road length: %d ; ", road.getLength());
         System.out.printf("2nd road length: %d\n", road1.getLength());
 
+        for (int i = 0; i < roads.length; car.setPosition(1), i++) { // Iterate for every road object created, resetting the car's position
+            for (;car.getPosition() <= roads[i].getLength(); car.move()) { // Loops while the car's position is lower than the current road's length
+
+                if (car.getPosition() == traffic_light.getPosition() - 1 && i == 0) // Sets traffic light to green when the car reaches the position before it and it's on the first road
+                {
+                    traffic_light.setStatus(); // Change the status of the traffic light to true
+                    for(int x = 0; x < 1000000000; ++x) // Add a delay
+                        for(int y = 0; y < 1000000000; ++y);
+                }
+
+                System.out.printf("Car: Road %s, Segment %d : %s\n", i+1, car.getPosition(), traffic_light.isStatus()); // Outputs the cars current position on the road
+            }
+        }
     }
 }
