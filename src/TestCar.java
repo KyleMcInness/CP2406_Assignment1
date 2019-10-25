a6e4394b82b0e195482bfe19b62874bffb6e1c2a
kyle <kyle.mcinness@my.jcu.edu.au>
2019-10-25 19:55:20 +1000

    Update TestCar class with new testing code

--- src/TestCar.java
+++ src/TestCar.java
@@ -1,19 +1,36 @@
+import model.Car;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCar {
-    @Test public void CheckTestCar() {
-        Car car = new Car();
-
-        assertEquals(1, car.getPosition());
-
-        car.setPosition(5);
-        assertEquals(5, car.getPosition());
-        car.move();
-        assertEquals(6, car.getPosition());
-
-
-
-
+    @Test
+    public void intialSetup() {
+        Car car = new Car(10, 100, 10, 20, Color.red);
+        assertEquals(10, car.getPosX());
+        assertEquals(100, car.getPosY());
+        assertEquals(10, car.getWidth());
+        assertEquals(20, car.getHeight());
+        assertEquals(Color.red, car.getColor());
     }
+
+    @Test
+    public void moveXDirection() {
+        Car car = new Car(10, 100, 10, 20, Color.red);
+        car.moveX();
+        assertEquals(11, car.getPosX());
+        assertEquals(100, car.getPosY());
+    }
+
+    @Test
+    public void moveYDirection() {
+        Car car = new Car(10, 100, 10, 20, Color.red);
+        car.setYDir(1);
+        car.moveY();
+        assertEquals(10, car.getPosX());
+        assertEquals(101, car.getPosY());
+    }
+
 }
