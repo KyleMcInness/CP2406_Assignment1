a4104c7d44761f99da58f6800ce505c24bd4c01b
kyle <kyle.mcinness@my.jcu.edu.au>
2019-10-26 21:16:24 +1000

    Add orientation variable

--- src/model/Road.java
+++ src/model/Road.java
@@ -5,24 +5,27 @@ import java.util.Random;
 
 public class Road extends Shape{
     private int height, width;
-    private int positionX, positionY;
+    private int x, y;
+    private Boolean is_horizontal;
 
-    Car car = new Car(10, 100, 10, 20, Color.red, 1, 0);
-
-    public Road(int x, int y, Color color) {
+    public Road(int x, int y, Color color, Boolean is_horizontal) {
         super(x, y, color);
-        this.width = 200;
-        this.height = 44;
+        this.x = x;
+        this.y = y;
+        this.is_horizontal = is_horizontal;
+        setDimensions();
     }
 
-    public void setHeight(int bus_length) //  sets the road to be a random length between 6 and 15
+    public void setDimensions() //  sets the road to be a random length between 6 and 15
     {
+        if (is_horizontal) {
+            this.width = 200;
+            this.height = 55;
+        } else if (!is_horizontal) {
+            this.width = 55;
+            this.height = 200;
+        }
 
-        Random random = new Random();
-        int random_int = random.nextInt(bus_length * 5); // Gets a random int between 0 and 15
-        while (random_int < bus_length * 2) // While the int is below the minimum road_length
-            random_int = random.nextInt(bus_length * 5);
-        this.height = random_int; // Assigns the int to the length of the road
     }
 
     public int getHeight() {
@@ -33,17 +36,12 @@ public class Road extends Shape{
         return this.width;
     }
 
-    public void setPosition() {
-        this.positionX = car.getPosX();
-        this.positionY = car.getPosY();
+    public int getX() {
+        return this.x;
     }
 
-    public int getPositionX() {
-        return x;
-    }
-
-    public int getPositionY() {
-        return y;
+    public int getY() {
+        return this.y;
     }
 
     @Override
