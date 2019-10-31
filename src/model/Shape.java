868b211746a9e0a7e2125ce66d3fe566b9e09efb
kyle <kyle.mcinness@my.jcu.edu.au>
2019-10-31 11:52:27 +1000

    Remove color parameter

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -1,15 +1,12 @@
-package model;
-
 import java.awt.*;
 
 public abstract class Shape {
     int x, y;
     Color color;
 
-    Shape(int x, int y, Color color) {
+    Shape(int x, int y) {
         this.x = x;
         this.y = y;
-        this.color = color;
     }
 
     public abstract void update(int boundaryWidth, int boundaryHeight, TrafficLight.State state);
