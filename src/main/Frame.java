63efaa219112fc3d98df0ddb8bb10c5b1b4b2c73
kyle <kyle.mcinness@my.jcu.edu.au>
2019-10-31 11:55:29 +1000

    Add Frame class

---
+++ src/main/Frame.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Frame extends JPanel {
+
+    Frame() {
+
+        JMenuBar menu_bar = new JMenuBar();
+
+        JMenu edit_mode = new JMenu("Edit Mode");
+        JMenuItem create = new JMenuItem("Create");
+        JMenuItem edit = new JMenuItem("Edit");
+        JMenuItem load = new JMenuItem("Load");
+        JMenuItem save = new JMenuItem("Save");
+        edit_mode.add(create);
+        edit_mode.add(edit);
+        edit_mode.add(load);
+        edit_mode.add(save);
+
+        JMenu sim_mode = new JMenu("Simulation Mode");
+        JMenuItem set_update_rate = new JMenuItem("Set Update Rate");
+        JMenuItem run = new JMenuItem("Run");
+        JMenuItem stop = new JMenuItem("Stop");
+        JMenuItem set_spawn_rate = new JMenuItem("Set Spawn Rate");
+        sim_mode.add(set_update_rate);
+        sim_mode.add(run);
+        sim_mode.add(stop);
+        sim_mode.add(set_spawn_rate);
+
+        menu_bar.add(edit_mode);
+        menu_bar.add(sim_mode);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+}
